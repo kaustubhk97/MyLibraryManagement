7bf3ca3700064c35fae8d13fd2b98e1793a33322
Kaustubh <kaustubhk743@gmail.com>
2023-05-27 12:14:36 +0530

    userlogin has been made

--- WebApplication2/userlogin.aspx.cs
+++ WebApplication2/userlogin.aspx.cs
@@ -4,19 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace WebApplication2
 {
     public partial class userlogin : System.Web.UI.Page
     {
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                SqlConnection con = new SqlConnection(constr);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                string q = "select * from member_master_tbl where username='" + TextBox1.Text.Trim() + "' AND password='" + TextBox2.Text.Trim() + "'";
+                SqlCommand cmd = new SqlCommand(q, con);
+                DataTable dt = new DataTable();
 
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                if (dt.Rows.Count >= 1)
+                {
+
+                }
+                else
+                {
+                    Response.Write("<script>alert(' please check login credentials once again ');</script>");
+                }
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
